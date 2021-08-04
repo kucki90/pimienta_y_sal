b632a8f88d8a77e38235647efaa95a013b3511ef
kucki90 <c.kuckiewicz@hotmail.com>
2021-08-04 01:48:23 -0300

    subidos cambios

---
+++ src/controllers/indexController.js
@@ -0,0 +1,17 @@
+const platos = require ('../data/platos_db')
+
+module.exports = {
+    index : (req,res) => {
+        return res.render('index', {
+            platos,
+        });
+    },
+    detail : (req,res) => {
+
+        let plato = platos.find(plato => plato.id === +req.params.id)
+
+        return res.render('detalleMenu', {
+            plato,
+        })
+    }
+}
